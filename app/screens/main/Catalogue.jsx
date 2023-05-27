e43172b9f565093ccfe9e34018a11c0aa2f49e84
getsquared <79702014+getsquared@users.noreply.github.com>
2023-05-27 23:59:31 +0800

    Add scrolling avatars in CATalogue
    Made cat avatar components (touchable and not) and vertical scroll for CATalogue.

--- app/screens/main/Catalogue.jsx
+++ app/screens/main/Catalogue.jsx
@@ -1,11 +1,24 @@
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 import { Text } from "react-native-paper";
+import { concernCatCards } from "../../components/CatCardData";
+import { TouchableCatAvatar } from "../../components/CatAvatar";
 
 export default function Catalogue() {
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>CATalogue</Text>
-            <Text>Work in Progress :D</Text>
-        </View>
+        <FlatList style={{ alignContent: "center" }}
+            ItemSeparatorComponent={() => <View style={{height: 20}} />}
+            ListHeaderComponent={<Text variant="headlineLarge">Meet the Cats!</Text>}
+            
+            data={concernCatCards}
+            renderItem={({item, index}) => {
+                return (
+                    <TouchableCatAvatar size={200}
+                        image={item.catImage}
+                        variant="headlineLarge"
+                        text={item.catName}
+                    />
+                )
+            }}
+        />
     );
 }
