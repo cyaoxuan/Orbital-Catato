f2d7a095efb3d0605b00424c2b76c700022be7d2
getsquared <79702014+getsquared@users.noreply.github.com>
2023-06-08 15:49:15 +0800

    Set up basic map

--- app/screens/main/Map.jsx
+++ app/screens/main/Map.jsx
@@ -1,11 +1,23 @@
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 export default function Map() {
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Map</Text>
-            <Text>Work in Progress :D</Text>
-        </View>
+        <View style={{ flex: 1 }}>
+            <MapView style={{ height: "100%" }}
+                provider={PROVIDER_GOOGLE}
+    
+                showsUserLocation={true}
+
+                region={{
+                    latitude: 1.296769,
+                    longitude: 103.776437,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                  }}>
+                
+                <Marker coordinate={{latitude: 1.296769, longitude: 103.776437,}} title="Meow!" />
+            </MapView>
+        </View>  
     );
 }
