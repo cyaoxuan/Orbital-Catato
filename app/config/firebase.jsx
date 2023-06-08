952fdb0d16e3fe8b56f5b709116d9a4fbc1f5d57
Yao Xuan <chuayaoxuan@gmail.com>
2023-06-08 15:29:50 +0800

    deploy app

--- app/config/firebase.jsx
+++ app/config/firebase.jsx
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: "AIzaSyDgph_4AdnIhptyqqxauAjWGNAfR5XGU1w",
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
