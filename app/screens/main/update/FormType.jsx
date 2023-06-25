c83750f9fd6ef367ed9bb0de26dbf148d652bcd2
Yao Xuan <chuayaoxuan@gmail.com>
2023-06-25 15:37:56 +0800

    fix: more bug fixes from e2e testing
    - "report new cat" is injured although it is healthy
    - update location.json to remove duplicates

--- app/screens/main/update/FormType.jsx
+++ app/screens/main/update/FormType.jsx
@@ -255,6 +255,7 @@ const ReportCat = (props) => {
                 lastSeenLocation: location,
                 lastSeenTime: date,
                 photoURI: photoURI,
+                concernStatus: concern,
                 concernDesc: concernDescription,
                 sterilised: sterile === "Yes",
             },
