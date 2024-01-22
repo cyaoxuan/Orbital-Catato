c783770214d739d4ddb77f62aa0cb3ff219f5953
Yao Xuan <69800289+cyaoxuan@users.noreply.github.com>
2024-01-22 15:28:42 +0800

    Update firebase.js

--- app/config/firebase.js
+++ app/config/firebase.js
@@ -5,22 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDgph_4AdnIhptyqqxauAjWGNAfR5XGU1w",
-//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.FIREBASE_API_KEY,
-//     appId: process.env.FIREBASE_API_KEY,
-// };
-
 const firebaseConfig = {
     apiKey: "AIzaSyDgph_4AdnIhptyqqxauAjWGNAfR5XGU1w",
-    authDomain: "catato-ea64f.firebaseapp.com",
-    projectId: "catato-ea64f",
-    storageBucket: "catato-ea64f.appspot.com",
-    messagingSenderId: "312029122334",
-    appId: "1:312029122334:web:910e9e8dc26a5ec02d030f",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_API_KEY,
+    appId: process.env.FIREBASE_API_KEY,
 };
 
 // Initialize Firebase
